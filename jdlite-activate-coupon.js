e84b2a1787700e0b5aa8b0280215dde281abb0aa
liyi <liyi1987@qq.com>
2022-04-01 15:15:11 +0800

    commit

--- jdlite-activate-coupon.js
+++ jdlite-activate-coupon.js
@@ -10,7 +10,6 @@
 // @run-at       document-start
 // ==/UserScript==
 
-// 修改页面初始数据
 (async function () {
   'use strict';
 
@@ -21,36 +20,37 @@
       return _data
     },
     set: function (obj) {
-      if (obj.activityData) {
+      try {
         let couponFloorList = obj.activityData.floorList.filter(function (fl) { return fl.template === 'free_coupon' })
         couponFloorList.forEach(function (fl) {
           fl.couponList.forEach(function (coupon) {
             coupon.status = '0'
           })
         })
-      }
+      } catch (e) {}
       _data = obj
     }
   });
 })();
 
-// 修改网络动态数据
 (function (fetch) {
   window.fetch = async function () {
     const res = await fetch.apply(this, arguments)
     console.log('fetch', arguments)
-    const json = await res.clone().json()
-    if (json.floorList) {
-      res.json = function () {
-        let couponFloorList = json.floorList.filter(function (fl) { return fl.template === 'free_coupon' })
-        couponFloorList.forEach(function (fl) {
-          fl.couponList.forEach(function (coupon) {
-            coupon.status = '0'
+    try {
+      const json = await res.clone().json()
+      if (json.floorList) {
+        res.json = function () {
+          let couponFloorList = json.floorList.filter(function (fl) { return fl.template === 'free_coupon' })
+          couponFloorList.forEach(function (fl) {
+            fl.couponList.forEach(function (coupon) {
+              coupon.status = '0'
+            })
           })
-        })
-        return Promise.resolve(json)
+          return Promise.resolve(json)
+        }
       }
-    }
+    } catch (e) {}
     return Promise.resolve(res)
   }
 })(window.fetch);
